5d521a039ef6a009f304d36c06e296c4da0bb8de
Tamim <tamim.rauful@gmail.com>
2024-10-29 17:21:58 +0600

    fragment added to bottom navigation

--- app/src/main/java/com/example/myapplica23/MainActivity.java
+++ app/src/main/java/com/example/myapplica23/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.myapplica23;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.SurfaceControl;
+import android.widget.Toast;
+
+import com.example.myapplica23.Fragment.AddFragment;
+import com.example.myapplica23.Fragment.HomeFragment;
+import com.example.myapplica23.Fragment.NotificationFragment;
+import com.example.myapplica23.Fragment.ProfileFragment;
+import com.example.myapplica23.Fragment.SearchFragment;
+import com.example.myapplica23.databinding.ActivityMainBinding;
+import com.iammert.library.readablebottombar.ReadableBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.readableBottomBar.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
+            @Override
+            public void onItemSelected(int i) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch (i){
+                    case 0:
+                        transaction.replace(R.id.container, new HomeFragment());
+                        break;
+                    case 1:
+                      transaction.replace(R.id.container, new NotificationFragment());
+                        break;
+                    case 2:
+                       transaction.replace(R.id.container, new AddFragment());
+                        break;
+                    case 3:
+                      transaction.replace(R.id.container, new SearchFragment());
+                        break;
+                    case 4:
+                     transaction.replace(R.id.container, new ProfileFragment());
+                        break;
+                }
+                transaction.commit();
+
+            }
+        });
     }
 }
