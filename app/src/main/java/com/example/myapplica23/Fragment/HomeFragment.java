6d41f3182eb2bc6f6eb1eba550196c6c34ec4cfb
Tamim <tamim.rauful@gmail.com>
2024-11-28 19:13:50 +0600

    story part complited

--- app/src/main/java/com/example/myapplica23/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/myapplica23/Fragment/HomeFragment.java
@@ -3,64 +3,55 @@ package com.example.myapplica23.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplica23.Adapter.StoryAdapter;
+import com.example.myapplica23.Model.StoryModel;
 import com.example.myapplica23.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+ RecyclerView storyRV;
+ ArrayList<StoryModel> list;
     public HomeFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        storyRV= view.findViewById(R.id.storyRV);
+        list = new ArrayList<>();
+
+        list.add(new StoryModel(R.drawable.story1, R.drawable.ic_back_svgrepo_com, R.drawable.ic_launcher_foreground, "John"));
+        list.add(new StoryModel(R.drawable.story2, R.drawable.ic_back_svgrepo_com, R.drawable.ic_launcher_foreground, "John"));
+        list.add(new StoryModel(R.drawable.story3, R.drawable.ic_back_svgrepo_com, R.drawable.ic_launcher_foreground, "John"));
+        list.add(new StoryModel(R.drawable.cover1, R.drawable.ic_back_svgrepo_com, R.drawable.ic_launcher_foreground, "John"));
+
+
+        StoryAdapter adapter=new StoryAdapter(list, getContext());
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        storyRV.setLayoutManager(layoutManager);
+        storyRV.setNestedScrollingEnabled(false);
+        storyRV.setAdapter(adapter);
+
+        return  view;
     }
 }
