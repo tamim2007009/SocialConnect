7b5ac20f20790119c67d321017e6fded7c4afebb
Tamim <tamim.rauful@gmail.com>
2024-12-20 20:03:50 +0600

    logout option added

--- app/src/main/java/com/example/myapplica23/MainActivity.java
+++ app/src/main/java/com/example/myapplica23/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.SurfaceControl;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.myapplica23.Fragment.AddFragment;
@@ -25,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        setSupportActionBar(binding.toolbar);
+        MainActivity.this.setTitle("My Profile");
+
+
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        binding.toolbar.setVisibility(View.GONE);
         transaction.replace(R.id.container, new HomeFragment());
         transaction.commit();
         binding.readableBottomBar.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
@@ -35,18 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (i){
                     case 0:
+                        binding.toolbar.setVisibility(View.GONE);
                         transaction.replace(R.id.container, new HomeFragment());
                         break;
                     case 1:
+                        binding.toolbar.setVisibility(View.GONE);
                       transaction.replace(R.id.container, new NotificationFragment());
                         break;
                     case 2:
+                        binding.toolbar.setVisibility(View.GONE);
                        transaction.replace(R.id.container, new AddFragment());
                         break;
                     case 3:
+                        binding.toolbar.setVisibility(View.GONE);
                       transaction.replace(R.id.container, new SearchFragment());
                         break;
                     case 4:
+                        binding.toolbar.setVisibility(View.VISIBLE);
                      transaction.replace(R.id.container, new ProfileFragment());
                         break;
                 }
@@ -54,5 +68,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
     }
 }
