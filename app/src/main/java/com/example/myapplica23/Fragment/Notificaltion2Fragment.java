72ffbd6261e4b282953443ee1ada5229b3b2a37d
Tamim <tamim.rauful@gmail.com>
2024-12-22 19:05:18 +0600

    Notification part completed

--- app/src/main/java/com/example/myapplica23/Fragment/Notificaltion2Fragment.java
+++ app/src/main/java/com/example/myapplica23/Fragment/Notificaltion2Fragment.java
@@ -3,64 +3,56 @@ package com.example.myapplica23.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplica23.Adapter.NotificationAdapter;
+import com.example.myapplica23.Model.NotificationModel;
 import com.example.myapplica23.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Notificaltion2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class Notificaltion2Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerView;
+    ArrayList<NotificationModel> list;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Notificaltion2Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Notificaltion2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Notificaltion2Fragment newInstance(String param1, String param2) {
-        Notificaltion2Fragment fragment = new Notificaltion2Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notificaltion2, container, false);
+
+
+        View view= inflater.inflate(R.layout.fragment_notificaltion2, container, false);
+        recyclerView = view.findViewById(R.id.notification2RV);
+        list = new ArrayList<>();
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        list.add(new NotificationModel(R.drawable.ic_launcher_foreground,"<b>Tamim </> Liked your post","1 hour ago"));
+        NotificationAdapter notificationAdapter = new NotificationAdapter(list,getContext());
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(notificationAdapter);
+        return  view;
+
     }
 }
