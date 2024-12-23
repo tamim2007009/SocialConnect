3e21c26c1b3fffe027a671819a910c23b7aa3893
Tamim <tamim.rauful@gmail.com>
2024-12-23 11:26:00 +0600

    Signin firebase done

--- app/src/main/java/com/example/myapplica23/LoginActivity.java
+++ app/src/main/java/com/example/myapplica23/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplica23;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,16 +8,56 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.myapplica23.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     ActivityLoginBinding binding;
+    FirebaseAuth auth;
+    FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+
+        currentUser = auth.getCurrentUser();
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = binding.emailET.getText().toString();
+                String password = binding.passwordET.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            binding.passwordET.setError("Invalid Password");
+                        }
+
+
+
+                    }
+                });
+
+
+            }
+        });
 
 
         binding.gotoSignup.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +68,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
