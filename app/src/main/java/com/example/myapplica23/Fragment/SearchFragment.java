15926bd74ff6cdc38cd83a2b5f054cc70bdcff8d
Tamim <tamim.rauful@gmail.com>
2025-04-29 01:27:43 +0600

    searching is done correctly

--- app/src/main/java/com/example/myapplica23/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/myapplica23/Fragment/SearchFragment.java
@@ -1,18 +1,19 @@
 package com.example.myapplica23.Fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.myapplica23.Adapter.UserAdapter;
 import com.example.myapplica23.Model.User;
-import com.example.myapplica23.R;
 import com.example.myapplica23.databinding.FragmentSearchBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -22,17 +23,16 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-
 public class SearchFragment extends Fragment {
     FragmentSearchBinding binding;
-    ArrayList<User> list = new ArrayList<>();
+    ArrayList<User> userList = new ArrayList<>();
+    UserAdapter adapter;
     FirebaseAuth auth;
     FirebaseDatabase database;
+
     public SearchFragment() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,37 +49,69 @@ public class SearchFragment extends Fragment {
 
         binding.usersRV.showShimmerAdapter();
 
-        UserAdapter adapter = new UserAdapter(getContext(),list);
+        adapter = new UserAdapter(getContext(), userList);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         binding.usersRV.setLayoutManager(layoutManager);
+        binding.usersRV.setAdapter(adapter);
 
-
+        // Fetch users from Firebase
         database.getReference().child("Users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                list.clear();
-                for (DataSnapshot dataSnapshot :
-                        snapshot.getChildren()) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     User user = dataSnapshot.getValue(User.class);
                     user.setUserID(dataSnapshot.getKey());
-                    if (!dataSnapshot.getKey().equals(FirebaseAuth.getInstance().getUid())){
-                        list.add(user);
+                    if (!dataSnapshot.getKey().equals(auth.getUid())) {
+                        userList.add(user);
                     }
                 }
 
-                binding.usersRV.setAdapter(adapter);
-                binding.usersRV.hideShimmerAdapter();
                 adapter.notifyDataSetChanged();
+                binding.usersRV.hideShimmerAdapter();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
 
+        // Search Text Change Listener
+        binding.searchMenuSearchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Not needed
+            }
+        });
 
         return binding.getRoot();
+    }
+
+    // Method to filter users
+    private void searchUser(String query) {
+        ArrayList<User> filteredList = new ArrayList<>();
+        for (User user : userList) {
+            if (user.getName() != null && user.getName().toLowerCase().contains(query.toLowerCase())) {
+                filteredList.add(user);
+            }
+        }
+
+        if (filteredList.isEmpty()) {
+            Toast.makeText(getContext(), "No User Found", Toast.LENGTH_SHORT).show();
+        }
+
+        adapter = new UserAdapter(getContext(), filteredList);
+        binding.usersRV.setAdapter(adapter);
     }
 }
