4f7bbf939e3734b03f435bea14e25d9dca30865e
Tamim <tamim.rauful@gmail.com>
2025-04-29 01:01:43 +0600

    signup and login modified

--- app/src/main/java/com/example/myapplica23/SignUpActivity.java
+++ app/src/main/java/com/example/myapplica23/SignUpActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.myapplica23.Model.User;
-import com.example.myapplica23.databinding.ActivityMainBinding;
 import com.example.myapplica23.databinding.ActivitySignUpBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,12 +18,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
-
-
     ActivitySignUpBinding binding;
-
     FirebaseAuth auth;
-
     FirebaseDatabase database;
 
     @Override
@@ -33,56 +28,48 @@ public class SignUpActivity extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
-         auth=FirebaseAuth.getInstance();
-         database=FirebaseDatabase.getInstance();
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.emailET.getText().toString();
+                String password = binding.passwordET.getText().toString();
 
-
-         binding.signupBtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-              String email= binding.emailET.getText().toString();
-                String password= binding.passwordET.getText().toString();
-
-
-                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(
+                                    binding.nameET.getText().toString(),
+                                    binding.professionET.getText().toString(), // <<< FIXED HERE
+                                    email,
+                                    password
+                            );
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(user);
 
+                            Toast.makeText(SignUpActivity.this, "User Data Saved", Toast.LENGTH_SHORT).show();
 
-                         if(task.isSuccessful()){
-                             User user =new User(binding.nameET.getText().toString(), binding.profession.getText().toString(), email, password);
-                             String id = task.getResult().getUser().getUid();
-                             database.getReference().child("Users").child(id).setValue(user);
-                             Toast.makeText(SignUpActivity.this, "user Data Saved", Toast.LENGTH_SHORT).show();
-
-
-
-                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-
-                         }
-                            else {
-                             Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                         }
-
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
-
-             }
-         });
-
-
+            }
+        });
 
         binding.gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
-
+                finish(); // optional: if you don't want user to come back to signup
             }
         });
     }
